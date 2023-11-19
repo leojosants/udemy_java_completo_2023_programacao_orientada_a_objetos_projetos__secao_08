452fdd592429ec130266335f8e38a123f1e16153
Leonardo Santos <leonardojosantos@gmail.com>
2023-11-18 22:23:18 -0300

    update file

--- ComecandoResolverUmSegundoProblemaExemplo/src/Main.java
+++ ComecandoResolverUmSegundoProblemaExemplo/src/Main.java
@@ -14,15 +14,15 @@ public class Main {
 
 		System.out.println("\nEntre com os dados do produto");
 
-		product.name = solicitarNome(scanner);
-		product.price = solicitarPreco(scanner);
-		product.quantity = solicitarQuantidade(scanner);
+		product.name = requestName(scanner);
+		product.price = requestPrice(scanner);
+		product.quantity = requestQuantity(scanner);
 
-		exibirDados(product);
-		solicitarQuantidadeDeProdutoParaAdicionarNoEstoque(scanner, product);
-		exibirDados(product);
-		solicitarQuantidadeDeProdutoParaRemoverNoEstoque(scanner, product);
-		exibirDados(product);
+		displayData(product);
+		requestQuantityOfProductToAddInStock(scanner, product);
+		displayData(product);
+		requestQuantityOfProductToRemoveFromStock(scanner, product);
+		displayData(product);
 	
 		scanner.close();
 		
@@ -32,39 +32,39 @@ public class Main {
 	/*
 	 * functions
 	 */
-	private static String solicitarNome(Scanner scanner) {
+	private static String requestName(Scanner scanner) {
 		String name = "";
 		System.out.print("Nome do produto: ");
 		name = scanner.nextLine().toUpperCase().trim();
 		return name;
 	}
 
-	private static double solicitarPreco(Scanner scanner) {
+	private static double requestPrice(Scanner scanner) {
 		double price = 0.0;
 		System.out.print("Preço do produto: ");
 		price = scanner.nextDouble();
 		return price;
 	}
 
-	private static int solicitarQuantidade(Scanner scanner) {
+	private static int requestQuantity(Scanner scanner) {
 		int quantity = 0;
 		System.out.print("Quantidade do produto: ");
 		quantity = scanner.nextInt();
 		return quantity;
 	}
 
-	private static void exibirDados(Product product) {
+	private static void displayData(Product product) {
 		System.out.println("\nExibindo dados do Produto");
 		System.out.println(product.toString());
 	}
 
-	private static void solicitarQuantidadeDeProdutoParaAdicionarNoEstoque(Scanner scanner, Product product) {
+	private static void requestQuantityOfProductToAddInStock(Scanner scanner, Product product) {
 		System.out.print("Entre com a quantidade de produtos a ser adicionado no estoque: ");
 		int quantityToBeAdded = scanner.nextInt();
 		product.addProducts(quantityToBeAdded);
 	}
 
-	private static void solicitarQuantidadeDeProdutoParaRemoverNoEstoque(Scanner scanner, Product product) {
+	private static void requestQuantityOfProductToRemoveFromStock(Scanner scanner, Product product) {
 		System.out.print("Entre com a quantidade de produtos a ser removido no estoque: ");
 		int quantityToBeRemoved = scanner.nextInt();
 		product.removeProducts(quantityToBeRemoved);
---
+++ ExercicioDeFixacao01/src/entities/Rectagle.java
@@ -0,0 +1,8 @@
+package entities;
+
+public class Rectagle {
+/*
+ * attributes
+ */
+//	public double wi
+}
