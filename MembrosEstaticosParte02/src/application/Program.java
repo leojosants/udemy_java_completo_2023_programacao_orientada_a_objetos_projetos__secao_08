fbec34a89de44305853f1f5b98dede6cff943d3f
Leonardo Santos <leonardojosantos@gmail.com>
2023-11-19 12:06:52 -0300

    version 3

---
+++ MembrosEstaticosParte02/src/application/Program.java
@@ -0,0 +1,55 @@
+package application;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+import util.Calculator;
+
+public class Program {
+
+	public static void main(String[] args) {
+
+		Locale.setDefault(Locale.US);
+
+		Scanner scanner = new Scanner(System.in);
+
+		double radius = 0.0;
+		double circunference = 0.0;
+		double volume = 0.0;
+
+		radius = requestRadius(scanner);
+		circunference = Calculator.calculateCircunference(radius);
+		volume = Calculator.calculateVolume(radius);
+
+		displayData(circunference, volume);
+
+		scanner.close();
+
+		System.out.println("\n-> fim do programa");
+	}
+
+	/*
+	 * functions
+	 */
+	private static double requestRadius(Scanner scanner) {
+		double radius = 0.0;
+
+		do {
+			System.out.print("\nInforme o valor do raio: ");
+			radius = scanner.nextDouble();
+
+			while (radius <= 0) {
+				System.out.print("-> campo 'valor do raio' não pode ser menor ou igual a 0: ");
+				radius = scanner.nextDouble();
+			}
+		} while (radius <= 0);
+
+		return radius;
+	}
+
+	private static void displayData(double circunference, double volume) {
+		System.out.printf("\nCircunferência:   %.2f%n", circunference);
+		System.out.printf("Volume:           %.2f%n", volume);
+		System.out.printf("PI:               %.2f%n", Calculator.PI);
+	}
+}
---
+++ MembrosEstaticosParte02/src/util/Calculator.java
@@ -0,0 +1,21 @@
+package util;
+
+public class Calculator {
+
+	public static final double PI = 3.14159;
+
+	/*
+	 * methods
+	 */
+	public static double calculateCircunference(double radius) {
+		double circunference = 0.0;
+		circunference = 2.0 * PI * radius;
+		return circunference;
+	}
+
+	public static double calculateVolume(double radius) {
+		double volume = 0.0;
+		volume = (4.0 * PI * Math.pow(radius, 3)) / 3.0;
+		return volume;
+	}
+}
